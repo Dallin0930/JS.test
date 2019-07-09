0adc1daac6b615ead8fc12ba869b833539ece1a8
DALLIN GUO (ITA-ISDC-ISD-OOCLL/ZHA) <GUODA@OOCL.COM>
2019-07-09 17:24:03 +0800

    multiple

--- __tests__/main-spec.js
+++ __tests__/main-spec.js
@@ -1,6 +1,18 @@
 const add = require('../main');
 
-it ('should add two numbers', () => {
-    expect(add(2, 3)).toBe(5);
+
+/* it ('should return result when startnumber and endnumber range from 1-1000', () => {
+    expect(isInRangeOf1to1000(2, 3)).toBe(false);
 });
+
+//乘法表对比输出
+it ('should return table when given two number', () => {
+    expect(printMultipleTable(2, 4)).toBe(caseTest);
+}); */
+
+//乘法表打印        
+printMultipleTable(startNum,endNum);
+
+
+
 
--- main.js
+++ main.js
@@ -1,5 +1,25 @@
-function add (left, right) {
-    return left + right;
+function isInRangeOf1to1000 (startNum, endNum) {
+    if(startNum>1&&startNum<1000&&endNum>1&&endNum<1000)
+    {
+          return true
+    }else{
+        return false
+    }
+   
 }
 
-module.exports = add;
+/* module.exports = isInRangeOf1to1000(2,3) */
+
+function printMultipleTable(startNum,endNum)
+{
+     for(var i=1;i<=startNum;i++)
+     {
+         for(var k=1;k<=endNum;k++) 
+         {
+             /* print(i +"*"+k+"="+i*k+"\n"); */
+             console.log(i +"*"+k+"="+i*k+"\n");
+
+         }
+     } 
+}  
+module.exports = printMultipleTable(2,4)
